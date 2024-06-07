47f6c1e3ffec4806db40b9999470b9006335e8a7
Lychee <itslychee@protonmail.com>
2024-06-07 18:15:51 -0500

    nix: logically split packaging to not be flake-dependent (#1)

--- default.nix
+++ default.nix
@@ -1,32 +1,4 @@
-{
-  rustPlatform,
-  gtk4,
-  glib,
-  polkit,
-  pkg-config,
-}: let
-  polkitAgentPath = "${polkit}/lib/polkit-1/polkit-agent-helper-1";
-  pname = "soteria";
-  version = "0.1.0";
-in
-  rustPlatform.buildRustPackage rec {
-    inherit pname version;
-    POLKIT_AGENT_HELPER_PATH = polkitAgentPath;
-
-    nativeBuildInputs = [
-      pkg-config
-    ];
-
-    buildInputs = [
-      gtk4
-      glib
-      polkit
-    ];
-    inherit glib;
-
-    src = ./.;
-
-    cargoLock = {
-      lockFile = ./Cargo.lock;
-    };
-  }
+{pkgs ? import <nixpkgs> {}}: rec {
+  soteria = pkgs.callPackage ./package.nix {};
+  default = soteria;
+}
