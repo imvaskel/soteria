638ff7935d19baccfe2200a4eb9c0dbcdf9ef5a2
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2025-01-18 14:36:06 -0500

    fix subject details for register

--- src/main.rs
+++ src/main.rs
@@ -90,14 +90,8 @@ async fn main() -> Result<()> {
     let subject_kind = "unix-process".to_string();
     let subject_details = HashMap::from([
         ("pid".to_string(), Value::new(std::process::id())),
-        (
-            "start-time".to_string(),
-            Value::new(
-                SystemTime::now()
-                    .duration_since(SystemTime::UNIX_EPOCH)?
-                    .as_millis() as u64,
-            ),
-        ),
+        ("start-time".to_string(), 0),
+        ("uid".to_string(), -1),
     ]);
     let subject = Subject::new(subject_kind, subject_details);
 
